f8b49d37c8d14b320fa10b1dd768b14cac9d6b64
Dan Harris <dan@sparkcode.co.uk>
2019-02-21 22:17:09 +0000

    Initial server build code

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-# ccsweb-frontend-image
-Website Phase 3
+# ccsweb-frontend-image - Frontend AMI build
+
+Packer-managed AWS AMI build of the frontend server for the CCS website.
---
+++ build.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+# Packer build script
+
+set -e
+
+# Update system packages
+sudo yum update -y
+
+# Add repos required for PHP 7.2
+sudo yum -y install https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
+sudo yum -y install https://centos7.iuscommunity.org/ius-release.rpm
+
+# Install required packages
+# NOTE: ruby required for CodeDeploy agent
+sudo yum -y install \
+    httpd \
+    ruby \
+    php72u \
+    php72u-mysqlnd.x86_64 \
+    php72u-opcache \
+    php72u-xml \
+    php72u-gd \
+    php72u-devel \
+    php72u-intl \
+    php72u-mbstring \
+    php72u-bcmath \
+    php72u-soap \
+    php72u-json
+
+# Apply custom apache config
+sudo mv -f ~ec2-user/httpd.conf /etc/httpd/conf/httpd.conf
+
+# Install CodeDeploy agent
+sudo curl -s -o install https://aws-codedeploy-eu-west-1.s3.amazonaws.com/latest/install
+sudo chmod +x install
+sudo ./install auto
+sudo rm -f install
